03a6d274375a7d4d861306a3a3900fb398e053b2
engin üstün <enginustun@outlook.com>
2018-03-31 03:36:09 +0300

    Add 'getMatchedWord' function to helper module.

--- dev/js/common/helper.js
+++ dev/js/common/helper.js
@@ -2,4 +2,13 @@ module.exports = {
   strings: {
     capitalizeFirstLetter: string => string.charAt(0).toUpperCase() + string.slice(1),
   },
+
+  /**
+   * Returns matched word from second array which has same index
+   * of 'text' parameter in first given array
+   */
+  getMatchedWord: (text, textArray = [], wordArray = []) => {
+    const textIndex = textArray.indexOf(text);
+    return textIndex > -1 ? wordArray[textIndex] : text;
+  },
 };
